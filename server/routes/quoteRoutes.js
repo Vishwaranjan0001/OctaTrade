2dd44c27915539706b52a4f4a2ecc3a2cd4e4cc4
Vishwaranjan Singh <vishwaranjansingh64@gmail.com>
2026-09-12 12:16:30 +0530

    feat: validate quote symbols

--- server/routes/quoteRoutes.js
+++ server/routes/quoteRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { getQuote } from "../Controllers/quoteController.js";
-
+import { validateSymbol} from "../middleware/validateSymbol.js";
 const router = Router();
 
-router.get("/:symbol", getQuote);
+router.get("/:symbol",validateSymbol, getQuote);
 
 export default router;
